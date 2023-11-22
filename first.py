9394cee6e6830a29c6c961c18efc394f6ba3b3ac
sashetye2 <140918220+sashetye2@users.noreply.github.com>
2023-11-22 09:52:21 +0530

    Update first.py

--- first.py
+++ first.py
@@ -7,4 +7,4 @@ print("This is for auto trigger of commit")
 print("Added one more change")
 print("Added one more commit enabled for webhook")
 print("Added one more commit enabled for try now")
-print("Added one more commit enabled for try after git polling logs 5 min polloing ")
+print("Added one more commit enabled for try after git polling logs 5 min polloing ")S
